539c76bf070a6e4977454097496e212c0970f4d8
Jeff Jennings <jjennings1519@gmail.com>
2024-01-24 06:49:30 +0100

    my solution to square_root_manual.py

--- leet_code/python_problems/square_root_from_scratch.py
+++ leet_code/python_problems/square_root_from_scratch.py
@@ -1,1 +1,18 @@
+import numpy as np
 
+def manual_sqrt(x, tol=1e-2, nsearch=100000):
+  """Manual square root routine for positive input 'x'. 
+  Determination of square root to within tolerance 'tol'."""
+  
+  search_space = np.linspace(0, x, nsearch)
+  remainders = []
+
+  for ii in search_space:
+    remainders.append(x - ii * ii)
+    if abs(remainders[-1]) < tol: 
+      print(f"square root of {x} is {ii} within tolerance {tol}")
+      return ii
+  
+  remainders = np.array(remainders)
+  print("square root within tolerance not found. closest result "
+        f"{search_space[np.argmin(abs(remainders))]} with remainder {min(abs(remainders))}")
